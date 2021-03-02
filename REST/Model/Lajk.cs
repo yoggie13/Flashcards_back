092381fdcf7fa70d8fee9ba9c71a11d6743aefbb
pantovicnevena <nevena.pantovic1998@gmail.com>
2021-03-02 23:26:47 +0100

    Dodat model

---
+++ REST/Model/Lajk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST.Model
+{
+    public class Lajk
+    {
+        public int PodkomentarID { get; set; }
+        public Komentar KomentarID { get; set; }
+        public Korisnik KorisnikID { get; set; }
+        public SkupKartica SkupKarticaID { get; set; }
+        public string Tekst { get; set; }
+        public Korisnik PodkomentarisaoID { get; set; }
+    }
+}
---
+++ REST/Model/Predmet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST.Model
+{
+    public class Predmete
+    {
+    }
+}
