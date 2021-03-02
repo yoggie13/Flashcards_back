86145877b50ca8f3f090671997e8e53a474a180e
StefanMil <stefan.milanovich@gmail.com>
2021-03-02 23:10:46 +0100

    Izmena 3

--- REST/Controllers/HttpRequestController.cs
+++ REST/Controllers/HttpRequestController.cs
@@ -26,7 +26,7 @@ namespace REST.Controllers
             switch (str)
             {
                 case "dashboard":
-                    return Ok("Hello");
+                    return Ok("Welcome");
                 default:
                     return NotFound("Nema");
             }
